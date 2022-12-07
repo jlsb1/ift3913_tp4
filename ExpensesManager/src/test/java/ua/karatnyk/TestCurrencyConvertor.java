8a0277fa5b2a13cc2946abb014ae9549d552e59a
jlsb1 <jeanloupberg@gmail.com>
2022-12-07 15:10:38 -0500

    Update TestCurrencyConvertor.java

--- ExpensesManager/src/test/java/ua/karatnyk/TestCurrencyConvertor.java
+++ ExpensesManager/src/test/java/ua/karatnyk/TestCurrencyConvertor.java
@@ -16,36 +16,105 @@ import static org.junit.Assert.*;
 
 import java.text.ParseException;
 
+import javax.swing.undo.StateEdit;
+
+import org.junit.Before;
+import org.junit.experimental.theories.suppliers.TestedOn;
 import org.junit.Before;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+import ua.karatnyk.impl.CurrencyConversion;
+import ua.karatnyk.impl.ExpensesProgramAPI;
+import ua.karatnyk.impl.JsonWorker;
 
 public class TestCurrencyConvertor {
-    private static CurrencyConversion conversion;
-    private static CurrencyConvertor convertor;
+    
+    private ExpensesProgramAPI test;
+    private CurrencyConversion conversion;
 
 
-    public static void blackBox() throws ParseException{
-        conversion = new JsonWorker().parser();
-        convertor = new CurrencyConvertor();
-        String[] devises = {"USD", "CAD", "GBP", "EUR", "CHF", "INR", "AUD"};
-        int devisesLenght = devises.length;
-        System.out.println(convertor.convert(33, "USD", "CAD", conversion));
-        for (int i = 0; i < devisesLenght; i++) {
-            for (int j = 0; j < devisesLenght; j++) {
-                if(i != j){
-                    System.out.println(devises[i]+" "+devises[j]);
-                    //test les motants
-                    Double normal = convertor.convert(50, devises[i], devises[j], conversion);
-                    Double below = convertor.convert(-1, devises[i], devises[j], conversion);
-                    Double over = convertor.convert(12333, devises[i], devises[j], conversion);
-                    System.out.println(normal+" "+below+" "+over);
-                }
-            }
-            System.out.println("----------------------");
-        }
+    public static double round(double d){
+        return (Math.round(d*100.0))/100.0;
+    }
+
+    @Before
+	public void init() {
+		test = new CurrencyConvertor();
+	}
+
+    @Test
+	public void conversionOfConversion(){
+        conversion  = new JsonWorker().parser();
+        Double a = test.convert(50, "USD", "CAD", conversion);
+        Double b = test.convert(a, "CAD", "USD", conversion);
+        boolean output = (50 == b);
+        assertTrue(output);
+	}
+
+    @Test
+    public void convertUnknownDevise(){
+        conversion  = new JsonWorker().parser();
+        assertNull(test.convert(50, "CAD", "MXN", conversion));
+    }
+
+    @Test
+    public void deviseSizeToBig(){
+        conversion  = new JsonWorker().parser();
+        assertNull(test.convert(50, "CAD", "USDD", conversion));
+    }
+
+    @Test
+    public void deviseSizeToBig2(){
+        conversion  = new JsonWorker().parser();
+        assertNull(test.convert(50, "CADD", "USD", conversion));
+    }
+
+    @Test
+    public void deviseSizeToSmall(){
+        conversion  = new JsonWorker().parser();
+        assertNull(test.convert(50, "CAD", "SD", conversion));
+    }
+
+    @Test
+    public void deviseSizeToSmall2(){
+        conversion  = new JsonWorker().parser();
+        assertNull(test.convert(50, "CAD", "USD", conversion));
+    }
+
+    @Test 
+    public void montantToSmall(){
+        conversion  = new JsonWorker().parser();
+        assertNull(test.convert(-12, "CAD", "USD", conversion));
+    }
+
+    @Test 
+    public void montantToBig(){
+        conversion  = new JsonWorker().parser();
+        assertNull(test.convert(14377, "CAD", "USD", conversion));
+    }
+
+    @Test 
+    public void montantLimit(){
+        conversion  = new JsonWorker().parser();
+        assertNotNull(test.convert(0, "CAD", "USD", conversion));
+    }
+
+    @Test 
+    public void montantUpperLimit(){
+        conversion  = new JsonWorker().parser();
+        assertNotNull(test.convert(10000, "CAD", "USD", conversion));
+    }
+
+    @Test 
+    public void gotToBig(){
+        conversion  = new JsonWorker().parser();
+        assertNull(test.convert(test.convert(9999, "USD", "CAD", conversion), "CAD", "USD", conversion));
     }
 
     public static void main(String[] args) throws ParseException{
-        blackBox();
+        
     }
 }
